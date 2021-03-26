8e3bbc6e9fa99c408f0211715ca237ea7c104d72
augustmeyer <75350319+augustmeyer@users.noreply.github.com>
2021-03-26 14:11:24 -0400

    updating js code to create table. still buggy

--- public/script.js
+++ public/script.js
@@ -1,13 +1,44 @@
-async function fetchHalls() {
-  const table = document.querySelector('.table');
+async function fetchAPI() {
+  const table = document.querySelector('.diningTable');
   
   const request = await fetch('/api/dining');
   const data = await request.json();
-  const halls = [];
+  const array = [];
 
+  array.push(data['data']);
+  console.log(array[0][1]['hall.id']);
 
-async function windowActions() {
-    fetchHalls();
+  function createDiningTable () {
+    const columns = `<thead>
+            <tr>
+              <th>Hall ID</th>
+              <th>Name</th>
+              <th>Loction</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <th>${halls[0][0]['hall.id']}</th>
+                <td>${halls[0][0]['hall.name']}</td>
+                <td>${halls[0][0]['hall.address']}</td>
+            </tr>
+
+            <tr>
+                <th>${halls[0][1]['hall.id']}</th>
+                <td>${halls[0][1]['hall.name']}</td>
+                <td>${halls[0][1]['hall.address']}</td>
+            </tr>
+
+            <tr>
+                <th>${halls[0][2]['hall.id']}</th>
+                <td>${halls[0][2]['hall.name']}</td>
+                <td>${halls[0][2]['hall.address']}</td>
+            </tr>
+          </tbody>`;
+    table.innerHTML = columns;
   }
-  
-window.onload = windowActions;
+  createDiningTable();
+}
+
+window.onload = fetchAPI;
